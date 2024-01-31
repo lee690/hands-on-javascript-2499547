368ab2dbce255a57356e1f02b357737cf9888acf
Christopher Lee <chrisleejean@gmail.com>
2024-01-31 20:44:54 +0000

    delete all end folders

--- 01_04e/script.js
+++
@@ -1,6 +0,0 @@
-import data from "./data.js";
-import Cardlist from "./components/Cardlist.js";
-
-const mainContent = document.querySelector(".main-content");
-
-mainContent.innerHTML = Cardlist(data);
